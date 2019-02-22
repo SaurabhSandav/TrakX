8850722534553235d0818619fd178f2f4ea6e2fe
Saurabh Sandav <justmailsaurabh@gmail.com>
2019-02-22 20:15:35 +0530

    ui.common.Utils: Add LayoutInflater.dataBindingInflate()
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/trakx/ui/common/Utils.kt
+++ app/src/main/java/com/redridgeapps/trakx/ui/common/Utils.kt
@@ -1,2 +1,15 @@
 package com.redridgeapps.trakx.ui.common
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+fun <T : ViewDataBinding> LayoutInflater.dataBindingInflate(
+    @LayoutRes resource: Int,
+    root: ViewGroup?,
+    attachToParent: Boolean = false
+): T {
+    return DataBindingUtil.inflate(this, resource, root, attachToParent)
+}
