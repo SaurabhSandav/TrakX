716055e5af4030c8a3c1c15c0ccf3a057e5579d1
Saurabh Sandav <justmailsaurabh@gmail.com>
2019-02-22 19:33:47 +0530

    Add DaggerFragmentFactory
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

---
+++ app/src/main/java/com/redridgeapps/trakx/ui/base/dagger/DaggerFragmentFactory.kt
@@ -0,0 +1,27 @@
+package com.redridgeapps.trakx.ui.base.dagger
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
+
+@Singleton
+class DaggerFragmentFactory @Inject constructor(
+    private val creators: Map<Class<out Fragment>, @JvmSuppressWildcards Provider<Fragment>>
+) : FragmentFactory() {
+
+    override fun instantiate(classLoader: ClassLoader, className: String, args: Bundle?): Fragment {
+
+        val modelClass = Class.forName(className).asSubclass(Fragment::class.java)
+
+        val creator = creators[modelClass] ?: creators.asIterable().firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value
+
+        creator ?: throw IllegalArgumentException("Unknown Fragment class $modelClass")
+
+        return creator.get()
+    }
+}
