111dfda5dcf7b30b97d338dafbe70d5776914a33
Saurabh Sandav <justmailsaurabh@gmail.com>
2019-02-22 19:40:11 +0530

    Prepare for using FragmentFactory with Navigation library
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/trakx/di/modules/FragmentBuilder.kt
+++ app/src/main/java/com/redridgeapps/trakx/di/modules/FragmentBuilder.kt
@@ -2,10 +2,12 @@ package com.redridgeapps.trakx.di.modules
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
+import com.redridgeapps.trakx.ui.base.CustomNavHostFragment
 import com.redridgeapps.trakx.ui.base.dagger.DaggerFragmentFactory
 import dagger.Binds
 import dagger.MapKey
 import dagger.Module
+import dagger.multibindings.IntoMap
 import kotlin.reflect.KClass
 
 @MustBeDocumented
@@ -23,4 +25,9 @@ abstract class FragmentBuilder {
 
     @Binds
     abstract fun bindFragmentFactory(factory: DaggerFragmentFactory): FragmentFactory
+
+    @Binds
+    @IntoMap
+    @FragmentKey(CustomNavHostFragment::class)
+    abstract fun bindCustomNavHostFragment(factory: CustomNavHostFragment): Fragment
 }
---
+++ app/src/main/java/com/redridgeapps/trakx/ui/base/CustomFragmentNavigation.kt
@@ -0,0 +1,45 @@
+package com.redridgeapps.trakx.ui.base
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.FragmentNavigator
+import androidx.navigation.fragment.NavHostFragment
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Navigator.Name("fragment")
+@Singleton
+class CustomFragmentNavigator(
+    private val fragmentFactory: FragmentFactory,
+    context: Context,
+    manager: FragmentManager,
+    containerId: Int
+) : FragmentNavigator(context, manager, containerId) {
+
+    override fun instantiateFragment(
+        context: Context,
+        fragmentManager: FragmentManager,
+        className: String,
+        args: Bundle?
+    ): Fragment {
+        return fragmentFactory.instantiate(context.classLoader, className, args)
+    }
+}
+
+@SuppressLint("Instantiatable")
+class CustomNavHostFragment @Inject constructor(
+    private val fragmentFactory: FragmentFactory
+) : NavHostFragment() {
+
+    override fun createFragmentNavigator() = CustomFragmentNavigator(
+        fragmentFactory,
+        requireContext(),
+        childFragmentManager,
+        id
+    )
+}
