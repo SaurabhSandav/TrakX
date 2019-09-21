5e6bd5fccaa9f4a49424d2764d8f1fe377dc1b67
Saurabh Sandav <justmailsaurabh@gmail.com>
2019-09-21 18:47:07 +0530

    Update Kotlin Serialization to 0.13.0
    Signed-off-by: Saurabh Sandav <justmailsaurabh@gmail.com>

--- app/src/main/java/com/redridgeapps/trakx/utils/serialization/LongDateSerializer.kt
+++ app/src/main/java/com/redridgeapps/trakx/utils/serialization/LongDateSerializer.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Decoder
 import kotlinx.serialization.Encoder
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
-import kotlinx.serialization.internal.NullableSerializer
+import kotlinx.serialization.internal.nullable
 import kotlinx.serialization.serializer
 import org.threeten.bp.Instant
 import org.threeten.bp.LocalDate
@@ -18,7 +18,7 @@ object LongDateSerializer : KSerializer<Long> {
 
     private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
     private val zoneId = ZONE_ID_UTC
-    private val serializer = NullableSerializer(String.serializer())
+    private val serializer = String.serializer().nullable
 
     override fun serialize(encoder: Encoder, obj: Long) {
         val convertedStr = if (obj == EventDate.NO_DATE.longDate) null else obj.asISODate()
